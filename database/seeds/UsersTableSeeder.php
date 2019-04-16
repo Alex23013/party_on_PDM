c6a96551cb94c8f47cd1257d0fb6341d1e31abf8
Maria <alecornejolupa2013@gmail.com>
2019-04-16 12:42:02 -0500

    making seeders for testing

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    DB::table('users')->insert([
+	               'name' => "Test",
+	               'last_name'=>"Master",
+	               'dni'=>"02345678",
+	               'email' => 'test_master@yopmail.com',
+	               'cellphone'=>"999888777",
+	               'password' => bcrypt('123456'),
+	               'avatar' => 'default.png',
+	               'validated'=>'1',
+	               'role'=>0,	               
+	           ]);
+
+	    DB::table('users')->insert([
+	               'name' => "Test",
+	               'last_name'=>"Doctor",
+	               'dni'=>"12345678",
+	               'email' => 'test_doctor@yopmail.com',
+	               'cellphone'=>"999888777",
+	               'password' => bcrypt('123456'),
+	               'avatar' => 'default.png',
+	               'validated'=>'1',
+	               'role'=>1,	               
+	           ]);
+
+	    DB::table('users')->insert([
+	               'name' => "Test",
+	               'last_name'=>"Triaje",
+	               'dni'=>"22345678",
+	               'email' => 'test_triaje@yopmail.com',
+	               'cellphone'=>"999888777",
+	               'password' => bcrypt('123456'),
+	               'avatar' => 'default.png',
+	               'validated'=>'1',
+	               'role'=>2,	               
+	           ]);
+
+	    DB::table('users')->insert([
+	               'name' => "Test",
+	               'last_name'=>"Paciente",
+	               'dni'=>"32345678",
+	               'email' => 'test_paciente@yopmail.com',
+	               'cellphone'=>"999888777",
+	               'password' => bcrypt('123456'),
+	               'avatar' => 'default.png',
+	               'validated'=>'1',
+	               'role'=>3,	               
+	           ]);
+    }
+}
