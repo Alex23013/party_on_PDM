be33607aaa84313e06cf09daa2e94069d12cb85c
Maria <alecornejolupa2013@gmail.com>
2019-05-17 13:15:09 -0500

    dataTable d_services

--- public/js/thingsAppears.js
+++ public/js/thingsAppears.js
@@ -1,26 +1,3 @@
-//document.getElementById('closeButton').style.visibility='hidden'; 
-
-function closeAppears() {
-    document.getElementById('closeButton').style.visibility = 'visible';
-}
-
-function submitImageAppears() {
-    document.getElementById('submitImageButtom').style.visibility = 'visible';
-}
-
-function roleSelectAppears() {
-    document.getElementById('roleSelect').style.visibility = 'visible';
-    document.getElementById('roleInput').value = 1;
-}
-
-function timepickerAppears() {
-    document.getElementById('timepickerInput').style.visibility = 'visible';
-    document.getElementById('allDayInput').checked=0; 
-}
-
-
-
-function closeDisappears() {
-    document.getElementById('closeButton').style.visibility = 'hidden';
-}
-
+function submitChosePartner() {
+    document.getElementById('chosePartner').style.visibility = 'visible';
+    }
