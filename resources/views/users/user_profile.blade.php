3e0f81555625e7709a9087b3ad65137f5ab81059
Maria <alecornejolupa2013@gmail.com>
2019-05-21 12:53:41 -0500

    redesigning detail/profile user view

--- resources/views/users/user_profile.blade.php
+++ resources/views/users/user_profile.blade.php
@@ -7,103 +7,134 @@
     height:200px;
     border-radius:150px;
   }
-  .p-top{
-    padding-top: 3%;
-  }
   .p-left{
     padding-left: 5%;
+  }
+  .subtitle{
+    padding-bottom: 1%;
   }
 </style>
 
 <div class="row">
   <div class="col-xs-12">
     <div class="box">
-            <div class="box-body p-left">
-            @if (Auth::user()->id == $user['id'])
+        <div class="box-body p-left">
+          @if (Auth::user()->id == $user['id'])
             <h2> Mi Perfil<br></h2>
-            @else
+          @else
             <h2> Información de usuario {{$user['name_role']}} <br></h2>
-            @endif
-            <div class="col-md-8 p-top">
-            <ul>
-              <li> Nombre: <b><?=$user['name']?></b></li>
-              <li> Apellido: <b><?=$user['last_name']?></b></li>
-              <li> DNI: <b><?=$user['dni']?></b></li>
-              <li> Email: <b><?=$user['email']?> </b></li>
-              <li> Celular: <b><?=$user['cellphone']?></b></li>
-              <li> Rol: <b> <?=$user['name_role']?> </b></li>
-
-              @if ($user['role']==1)
-                <br>
-                <b>Información adicional por ser <?=$user['name_role']?> </b>
-                <li> Fecha de Nacimiento: <b> {{$s_user->birth_at}} </b></li>
-                <li> Dirección: <b> {{$s_user->address}} </b></li>
-                <li> Colegiatura: <b> 
-                @if($s_user->college)
-                <?=$s_user->college?>  
-                @else
-                No tiene
-                @endif 
-                </b></li>
-                <li> Especialidad: <b> <?=$s_user->specialty?> </b></li>
-                <li> Disponible: <b> 
-                @if($s_user->available)
-                Si
-                @else
-                No
-                @endif 
-                </b></li>
-                <br>
-                <b>Información adicional del contacto de emergencia </b>
-                <li> Nombre del contacto: <b><?=$s_user->ec_name?></b></li>
-                <li> Apellido del contacto: <b><?=$s_user->ec_last_name?></b></li>
-                <li> Celular del contacto: <b> {{$s_user->ec_cellphone}}</b></li>
-              @endif
-              
-              @if ($user['role']==2)
-              <br>
-              <b>Información adicional por ser de <?=$user['name_role']?> </b>
-              <li> Es médico: <b> 
-                @if($s_user->is_a_doctor)
-                Si
-                @else
-                No
-                @endif 
-              </b></li>
-              <li> Colegiatura: <b> 
-                @if($s_user->college)
-                {{$s_user->college}}  
-                @else
-                No tiene
-                @endif 
-              </b></li>
-              @endif
-              @if ($user['role']==3)
-              <br>
-              <b>Información adicional por ser <?=$user['name_role']?> </b>
-              <li> Fecha de nacimiento: <b> 
-                {{$s_user->birth_at}}
-              </b></li>
-              <br>
-                <b>Información adicional del contacto de emergencia </b>
-                <li> Nombre del contacto: <b><?=$s_user->ec_name?></b></li>
-                <li> Apellido del contacto: <b><?=$s_user->ec_last_name?></b></li>
-                <li> Celular del contacto: <b> {{$s_user->ec_cellphone}}</b></li>
-              @endif
-            </ul>
-
-              @if ($user->id == Auth::user()->id)
-              <a href="/users/profile/edit">  <button type="button" class="btn bg-purple margin">  <i class="fa fa-edit"></i>  Editar Usuario</button></a>
-              @if ($s_user)
-              <a href="/users/especific/edit"> <button class="btn  btn-flat bg-olive m-left">  <i class="fa fa-edit"></i> Editar información de <?=$user['name_role']?> </button> </a>
-              @endif
-              @endif
-              </div>
-              <div class="col-md-4 ">
-                  <img src="{{$url_image}}" class= "imgAvatar">
-              </div>    
+          @endif
+          <div class="col-md-8 ">
+            <div class="col-md-12" >
+              <span class="col-md-4"> Nombre: </span>
+              <label  class="col-md-8">{{$user->name}} </label>
             </div>
-            <!-- /.box-body -->
+            <div class="col-md-12" >
+              <span class="col-md-4"> Apellido: </span>
+              <label  class="col-md-8">{{$user->last_name}} </label>
+            </div>
+            <div class="col-md-12" >
+              <span class="col-md-4"> DNI: </span>
+              <label  class="col-md-8">{{$user->dni}} </label>
+            </div>
+            <div class="col-md-12" >
+              <span class="col-md-4"> Email: </span>
+              <label  class="col-md-8">{{$user->email}} </label>
+            </div>
+            <div class="col-md-12" >
+              <span class="col-md-4"> Celular: </span>
+              <label  class="col-md-8">{{$user->cellphone}} </label>
+            </div>
+            <div class="col-md-12" >
+              <span class="col-md-4"> Rol: </span>
+              <label  class="col-md-8">{{$user->name_role}} </label>
+            </div>
+            @if ($user['role']!=0)
+              <div class="col-md-12" >
+                <h4><label class="col-md-11 subtitle">Información adicional por ser {{$user->name_role}}</label></h4>
+              </div>
+              @if ($user['role']==2)
+                <div class="col-md-12" >
+                  <span class="col-md-4"> Es médico: </span>
+                  @if($s_user->is_a_doctor)
+                    <label  class="col-md-8"> Si </label>
+                  @else
+                    <label  class="col-md-8"> No </label>
+                  @endif
+                </div>
+                <div class="col-md-12" >
+                  <span class="col-md-4"> Colegiatura: </span>
+                  @if($s_user->college)
+                  <label  class="col-md-8"> {{$s_user->college}} </label>
+                  @else
+                  <label  class="col-md-8"> No tiene </label>
+                  @endif
+                </div>
+              @endif
+              @if ($user['role']==3  || $user['role']==1 ) 
+              <div class="col-md-12" >
+                <span class="col-md-4"> Fecha de Nacimiento: </span>
+                <label  class="col-md-8">{{$s_user->birth_at}} </label>
+              </div>
+              @if ($user['role']==1)
+                <div class="col-md-12" >
+                  <span class="col-md-4"> Dirección: </span>
+                  <label  class="col-md-8">{{$s_user->address}} </label>
+                </div>
+                <div class="col-md-12" >
+                  <span class="col-md-4"> Colegiatura: </span>
+                  @if($s_user->college)
+                    <label  class="col-md-8"> {{$s_user->college}} </label>
+                  @else
+                    <label  class="col-md-8"> No tiene </label>
+                  @endif
+                </div>
+                <div class="col-md-12" >
+                  <span class="col-md-4"> Especialidad: </span>
+                  <label  class="col-md-8">{{$s_user->specialty}} </label>
+                </div>
+                <div class="col-md-12" >
+                  <span class="col-md-4"> Disponible: </span>
+                  @if($s_user->available)
+                    <label  class="col-md-8"> Si </label>
+                  @else
+                    <label  class="col-md-8"> No </label>
+                  @endif
+                </div>
+              @endif
+              <div class="col-md-12" >
+                <h4> <label class="col-md-11 subtitle">Información adicional del contacto de emergencia</label></h4>
+              </div>
+              <div class="col-md-12" >
+                <span class="col-md-4"> Nombre del contacto: </span>
+                <label  class="col-md-8">{{$s_user->ec_name}} </label>
+              </div>
+              <div class="col-md-12" >
+                <span class="col-md-4"> Apellido del contacto: </span>
+                <label  class="col-md-8">{{$s_user->ec_last_name}} </label>
+              </div>
+              <div class="col-md-12" >
+                <span class="col-md-4"> Celular del contacto: </span>
+                <label  class="col-md-8">{{$s_user->ec_cellphone}} </label>
+              </div>
+              @endif
+            @endif
           </div>
+
+          <div class="col-md-4 ">
+              <img src="{{$url_image}}" class= "imgAvatar">
+          </div> 
+          <div class="col-md-12">
+              <div class="col-md-4"></div>
+              @if ($user->id == Auth::user()->id)
+                <a href="/users/profile/edit">  <button type="button" class="btn bg-purple margin">  <i class="fa fa-edit"></i>  Editar Usuario</button></a>
+                @if ($s_user)
+                  <a href="/users/especific/edit"> <button class="btn  btn-flat bg-olive m-left">  <i class="fa fa-edit"></i> Editar información de <?=$user['name_role']?> </button> </a>
+                @endif
+              @endif
+          </div>   
+        </div>
+        <!-- /.box-body -->
+    </div>
 </div>
 @endsection
