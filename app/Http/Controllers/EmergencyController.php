0fd78b10f95f76ead76d0717fcb4f19d30dfbbc4
Maria <alecornejolupa2013@gmail.com>
2019-05-23 15:14:41 -0500

    emergency adding

--- app/Http/Controllers/EmergencyController.php
+++ app/Http/Controllers/EmergencyController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
 use App\Attention;
 use App\Emergency;
+use App\Patient;
 
 class EmergencyController extends Controller
 {
@@ -27,9 +28,54 @@ class EmergencyController extends Controller
         $user_patient = $attention->patient->user;
     	return view('attentions.attention_detail')->with(compact('s_attention','attention','user_patient'));
     }
-    public function add(){}
+    public function add(){
+        $patients = Patient::all();
+        return view('emergencies.new_emergency')->with(compact('patients'));   
+    }
 
-    public function store(Request $request){}
+    public function store(Request $request){
+        //dd($request->all());
+        $rules = [
+                'patient_id' => 'required',
+                'motive' => 'required',
+                'caller_name'=>'required',
+                'caller_dni' => 'required|size:8',
+                'caller_cell' => 'required'
+            ];
+        $messages = [
+                'patient_id.required' => 'Es necesario ingresar un id de paciente para registrar una emergencia',
+                'motive.required' => 'Es necesario ingresar la descripcion del probelma del paciente para registrar una emergencia',
+                'caller_name.required' => 'Es necesario ingresar el nombre de la persona que llama para registrar una emergencia',
+                'caller_dni.required' => 'Es necesario ingresar el DNI de la persona que llama para registrar una emergencia',
+                'caller_cell.required' => 'Es necesario ingresar el número celular de la persona que llama para registrar una emergencia',
+            ];  
+        $this->validate($request, $rules, $messages);  
+        
+        $attention= New Attention;
+        $attention->patient_id = $request->patient_id;
+        $attention->motive = $request->motive;
+        $attention->attention_code = "AT-".str_random(3);
+        $attention->address = $request->address;
+        $attention->reference = $request->reference;
+        $attention->type = 2;
+        $attention->save();
+
+        $emergency = New Emergency;
+        $emergency->attention_id =$attention->id;
+        $emergency->caller_name = $request->caller_name;
+        $emergency->caller_dni =  $request->caller_dni;
+        $emergency->caller_cell =  $request->caller_cell;
+        $emergency->save();
+
+        //falta oc
+        $emergencies = DB::table('attentions')
+                    ->join('emergencies','attentions.id','=','emergencies.attention_id')
+                    ->join('patients','attentions.patient_id','=','patients.id')
+                    ->join('users','patients.user_id','=','users.id')
+                    ->get();
+        $new = NULL;   
+        return view('emergencies.emergency_index')->with(compact('emergencies','new'));
+    }
 
     public function delete($id){
     	$attention = Attention::find($id);
---
+++ resources/views/emergencies/new_emergency.blade.php
@@ -0,0 +1,141 @@
+@extends('layouts.template')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Registrar una Emergencia</div>
+                <div class="panel-body">
+                @if (count($errors) > 0)
+                      <div class="alert alert-danger">
+                          <ul>
+                              @foreach ($errors->all() as $error)
+                                  <li>{{ $error }}</li>
+                              @endforeach
+                          </ul>
+                      </div>
+                @endif
+                    <form class="form-horizontal" role="form" method="POST" action="/emergency">
+                         {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="patient_id" class="col-md-4 control-label">ID del paciente *</label>
+
+                            <div class="col-md-6">
+                                <input id="patient_id" type="text" class="form-control" name="patient_id" >
+                            </div>
+                           <!-- <select class="form-control" name = "patient_id" >
+                                    <option value=""> Seleccione un paciente </option>
+                                    @foreach($patients as $patient)
+                                    <option value="<?=$patient->id?>"><?=$patient->patient_code?></option>
+                                    @endforeach
+                            </select>-->
+                        </div>
+
+                        <div class="form-group">
+                            <label for="motive" class="col-md-4 control-label">Motivo  *</label>
+
+                            <div class="col-md-6">
+                                <textarea class="form-control" name = "motive"rows="3" placeholder="Describa el problema"></textarea>
+                                <b>Nota: Los campos con * son obligatorios</b>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="address" class="col-md-4 control-label">Dirección </label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control" name="address" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="reference" class="col-md-4 control-label">Referencia </label>
+
+                            <div class="col-md-6">
+                                <input id="reference" type="text" class="form-control" name="reference" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-8 control-label">Información de la persona que está llamando </label>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="caller_name" class="col-md-4 control-label">Nombre * </label>
+
+                            <div class="col-md-6">
+                                <input id="caller_name" type="text" class="form-control" name="caller_name" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="caller_dni" class="col-md-4 control-label">DNI *</label>
+
+                            <div class="col-md-6">
+                                <input id="caller_dni" type="text" class="form-control" name="caller_dni" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="caller_cell" class="col-md-4 control-label">Celular * </label>
+
+                            <div class="col-md-6">
+                                <input id="caller_cell" type="text" class="form-control" name="caller_cell" >
+                                <b>Nota: Los campos con * son obligatorios</b>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-8 control-label">Información de un contacto adicional (opcional) </label>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="oc_name" class="col-md-4 control-label">Nombre del contacto</label>
+
+                            <div class="col-md-6">
+                                <input id="oc_name" type="text" class="form-control" name="oc_name" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="oc_cell" class="col-md-4 control-label">Celular del contacto</label>
+
+                            <div class="col-md-6">
+                                <input id="oc_cell" type="text" class="form-control" name="oc_cell" >
+
+                            </div>
+                        </div>
+                         <div class="form-group">
+                            <label for="oc_relationship" class="col-md-4 control-label">Relación con el paciente</label>
+
+                            <div class="col-md-6">
+                                <input id="oc_relationship" type="text" class="form-control" name="oc_relationship" >
+
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-user"></i> Registrar
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('specific scripts')
+<script>
+  $('#datepicker').datepicker({
+      autoclose: true,
+      format: 'yyyy/mm/dd'
+    })
+</script>
+
+@endsection
