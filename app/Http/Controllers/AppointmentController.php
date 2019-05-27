fc3e71dd06496aa72fbe4aec855443ca19cf99ea
Maria <alecornejolupa2013@gmail.com>
2019-05-26 19:20:12 -0500

    adding an appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
 use App\Attention;
+use App\User;
+use App\Patient;
+use App\Doctor;
 use App\Appointment;
+use App\Specialty;
 
 class AppointmentController extends Controller
 {
@@ -28,9 +32,77 @@ class AppointmentController extends Controller
     	$user_patient = $attention->patient->user;
         return view('attentions.attention_detail')->with(compact('s_attention','attention','user_patient','specialty','user_doctor'));
     }
-    public function add(){}
+    public function add(){
+        $specialties = Specialty::all();
+        $one = NULL;
+        return view('appointments.new_appointment')->with(compact('specialties','one'));   
+    }
 
-    public function store(Request $request){}
+    public function store(Request $request){
+        if($request->choseSpecialty){
+            
+            $rules = [
+            'specialty_id' => 'required',
+            ];
+            $messages = [
+            'specialty_id.required' => 'Es necesario seleccionar una "especialidad"  para registrar una cita médica'
+            ];
+
+            $this->validate($request, $rules, $messages);
+            
+            $specialties = Specialty::find($request->specialty_id);
+            $one = 1;
+            $users = User::all();
+            $doctors =[];
+            $patients =  [];
+            foreach ($users as $user ) {
+                if($user->role == 1 ){
+                    if($user->doctor->specialty == $specialties->name){
+                        $doctors[]=array(
+                        "name" => $user->name,
+                        "id" => $user->id,
+                    );    
+                    }
+                }
+                if($user->role == 3){
+                    $patients[]=array(
+                        "dni" => $user->dni,
+                        "name" => $user->name,
+                        "id" => $user->id,
+                    );
+                }
+            }
+
+            return view('appointments.new_appointment')->with(compact('patients', 'doctors', 'specialties','one'));
+        }else{ //store an appointment
+            $attention = new Attention;
+            $patient = User::find($request->patient_user_id)->patient;
+            $attention->patient_id = $patient->id;
+            $attention->motive = $request->motive;
+            $attention->attention_code = "AT-".str_random(3);
+            //cambiar al tamaño a 9? eso hay que preguntar
+            $attention->address = $request->address;
+            $attention->reference = $request->reference;
+            $attention->type = 1;
+            $attention->save();
+
+            $appointment = New Appointment;
+            $appointment->attention_id = $attention->id;
+            $appointment->specialty_id = $request->specialty_id;
+            $doctor = User::find($request->doctor_user_id)->doctor;
+            $appointment->doctor_id = $doctor->id;
+            $appointment->date_time = $request->date." ".$request->time;
+            $appointment->save(); 
+
+            $appointments = DB::table('attentions')
+                    ->join('appointments','attentions.id','=','appointments.attention_id')
+                    ->join('patients','attentions.patient_id','=','patients.id')
+                    ->join('users','patients.user_id','=','users.id')
+                    ->get();
+            $new = $appointment;   
+            return view('appointments.appointment_index')->with(compact('appointments','new'));
+        }
+    }
 
     public function delete($id){
     	$attention = Attention::find($id);
---
+++ resources/views/appointments/new_appointment.blade.php
@@ -0,0 +1,160 @@
+@extends('layouts.template')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Registrar una Cita Médica a un paciente registrado</div>
+                <div class="panel-body">
+                @if (count($errors) > 0)
+                      <div class="alert alert-danger">
+                          <ul>
+                              @foreach ($errors->all() as $error)
+                                  <li>{{ $error }}</li>
+                              @endforeach
+                          </ul>
+                      </div>
+                @endif
+                    <form class="form-horizontal" role="form" method="POST" action="/appointments">
+                         {{ csrf_field() }}
+                         
+                         <div class="form-group">
+                            <label for="specialty" class="col-md-4 control-label">Especialidad *</label>
+
+                            <div class="col-md-6">
+                                @if($one)
+                                   <b> {{$specialties->name}}</b>
+                                   <input id="specialty_id" type="hidden" name="specialty_id" value ="{{$specialties->id}}">
+                                @else
+                                <select class="form-control" name = "specialty_id" >
+                                <option value=""> Seleccione una especialidad </option>
+                                @foreach($specialties as $specialty)
+                                  <option value="<?=$specialty->id?>"><?=$specialty->name?></option>
+                                @endforeach
+                                </select>
+                                @endif
+                            </div>
+                        </div>
+
+                         <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary " style="margin-left: 35%; margin-bottom: 1%;" name="choseSpecialty" value = "1">
+                                <i class="fa fa-stethoscope"></i> Elegir Especialidad
+                                </button>
+                            </div>
+                        @if ($one) 
+                        <div class="form-group">
+                            <label for="patient_id" class="col-md-4 control-label">Nombre del paciente *</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control" name = "patient_user_id" >
+                                <option value=""> Seleccione un paciente </option>
+                                @foreach($patients as $patient)
+                                  <option value="<?=$patient['id']?>"><?=$patient['name']?></option>
+                                @endforeach
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="motive" class="col-md-4 control-label">Motivo  *</label>
+
+                            <div class="col-md-6">
+                                <textarea class="form-control" name = "motive" rows="3" placeholder="Describa el problema"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="address" class="col-md-4 control-label">Dirección *</label>
+
+                            <div class="col-md-6">
+                                <input id="address" type="text" class="form-control" name="address" >
+                                 
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="reference" class="col-md-4 control-label">Referencia </label>
+
+                            <div class="col-md-6">
+                                <input id="reference" type="text" class="form-control" name="reference" >
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-8 control-label">Información de la cita </label>
+                        </div>
+
+                        <div class="form-group">
+
+                            <label for="doctor_id" class="col-md-4 control-label">Doctor *</label>
+
+                            <div class="col-md-6">
+                                
+                                
+                                @if($doctors)
+                                <select class="form-control" name = "doctor_user_id" >
+                                <option value=""> Seleccione un doctor </option>
+                                @foreach($doctors as $doctor)
+                                  <option value="<?=$doctor['id']?>"><?=$doctor['name']?></option>
+                                @endforeach
+                                </select>
+                                @else
+                                <option value=""> No hay doctores para esta especialidad </option>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="birth" class="col-md-4 control-label">Fecha de la cita * </label>
+
+                            <div class="col-md-6">
+                                <input id="datepicker" type="text" class="form-control" name="date" >
+                            </div>
+                        </div> 
+
+                        
+                        <div class="form-group">
+                            <label for="birth" class="col-md-4 control-label">Hora de la cita * </label>
+                            <div class="col-md-6">
+                            <div class="bootstrap-timepicker">
+                             <input id="schedule_start" type="text" class="form-control timepicker" name="time" >
+                                </div>
+                            <b>Nota: Los campos con * son obligatorios</b>
+                            </div>
+                        </div>
+                         
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary" name="registrar" value = "1">
+                                    <i class="fa fa-btn fa-user"></i> Registrar
+                                </button>
+                            </div>
+                        </div>
+                        @endif
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@section('specific scripts')
+<script type="text/javascript" >
+   $(function () {
+    //Timepicker
+    $('.timepicker').timepicker({
+      showInputs: false,
+      showSeconds: false,
+      showMeridian:false
+    })
+
+    $('#datepicker').datepicker({
+      autoclose: true,
+      format: 'yyyy/mm/dd'
+    })
+
+    }); 
+</script>
+@endsection
