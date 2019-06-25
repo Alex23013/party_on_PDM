2cbe2984883dd4f8f90dc73ae10a314f463bdd47
Maria <alecornejolupa2013@gmail.com>
2019-06-24 19:38:04 -0500

    medicines and recipes and kits (migrations and models)

---
+++ database/migrations/2019_06_24_192330_create_recipes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('appointment_id')->unsigned();
+            $table->json('medicines');
+            $table->text('instructions');
+            $table->timestamps();
+            $table->foreign('appointment_id')->references('id')->on('appointments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recipes');
+    }
+}
