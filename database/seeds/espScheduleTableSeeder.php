9e4c8c59b7af94f7477d9ed08f0b2497b9903a1a
Maria <alecornejolupa2013@gmail.com>
2019-06-27 10:20:32 -0500

    dataseeder fix

--- database/seeds/espScheduleTableSeeder.php
+++ database/seeds/espScheduleTableSeeder.php
@@ -15,7 +15,7 @@ class espScheduleTableSeeder extends Seeder
                    // 'id'=>11,
                    'name' => "CArdialogia",
                    'last_name'=>"Test_esp",
-                   'dni'=>"12345600",
+                   'dni'=>"12345609",
                    'email' => 'test_card@yopmail.com',
                    'cellphone'=>"999888777",
                    'password' => bcrypt('123456'),
@@ -27,7 +27,7 @@ class espScheduleTableSeeder extends Seeder
         $max_id_user = DB::statement("SELECT MAX(id) FROM users");
         DB::table('doctors')->insert([
                 //'id'=>3,
-                'user_id'=> $max_id_user ,
+                'user_id'=> $max_id_user+1 ,
                 'birth_at'=>"2019-04-16",
                 'address'=>"su casita",
                 'specialty_id'=> 2,             
@@ -38,7 +38,7 @@ class espScheduleTableSeeder extends Seeder
                    'name' => "Pediatra",
                    'last_name'=>"Test_esp",
                    'dni'=>"12345601",
-                   'email' => 'test_ciru@yopmail.com',
+                   'email' => 'test_pedi@yopmail.com',
                    'cellphone'=>"999888777",
                    'password' => bcrypt('123456'),
                    'avatar' => 'default.png',
@@ -46,12 +46,12 @@ class espScheduleTableSeeder extends Seeder
                    'role'=>1,
                    'name_role' => 'Doctor',                
                ]);
-        $max_id_user = DB::statement("SELECT MAX(id) FROM users");
+        //$max_id_user = DB::statement("SELECT MAX(id) FROM users");
         DB::table('doctors')->insert([
                 //'id'=>4,
                 //'user_id'=> 12,
-                'user_id'=> $max_id_user ,
-                'birth_at'=>"2019-24-16",
+                'user_id'=> $max_id_user +2,
+                'birth_at'=>"2019-04-16",
                 'address'=>"su casita",
                 'specialty_id'=> 3,             
             ]);
@@ -69,12 +69,12 @@ class espScheduleTableSeeder extends Seeder
                    'role'=>1,
                    'name_role' => 'Doctor',                
                ]);
-        $max_id_user = DB::statement("SELECT MAX(id) FROM users")
+        //$max_id_user = DB::statement("SELECT MAX(id) FROM users");
         DB::table('doctors')->insert([
                 //'id'=>4,
                 //'user_id'=> 13,
-                'user_id'=> $max_id_user ,
-                'birth_at'=>"2019-14-16",
+                'user_id'=> $max_id_user+3 ,
+                'birth_at'=>"2019-09-16",
                 'address'=>"su casita",
                 'specialty_id'=> 4,             
             ]);
@@ -82,10 +82,10 @@ class espScheduleTableSeeder extends Seeder
         DB::statement("SELECT SETVAL('users_id_seq', (SELECT MAX(id) FROM users))");
         DB::statement("SELECT SETVAL('doctors_id_seq', (SELECT MAX(id) FROM doctors))");
 
-        $max_id_doctor = DB::statement("SELECT MAX(id) FROM users")
+        $max_id_doctor = DB::statement("SELECT MAX(id) FROM doctors");
         DB::table('espschedules')->insert([  
         		"id"=>1,
-        		"doctor_id" =>$max_id_doctor-1,
+        		"doctor_id" =>$max_id_doctor+1,
         		"date"=>'2019-07-03 ',
         		"start_time"=>'14:52:16',
         		"end_time"=>'14:59:16',
@@ -93,7 +93,7 @@ class espScheduleTableSeeder extends Seeder
 	           ]);
         DB::table('espschedules')->insert([  
                 "id"=>2,
-                "doctor_id" =>$max_id_doctor-2,
+                "doctor_id" =>$max_id_doctor+2,
                 "date"=>'2019-07-02 ',
                 "start_time"=>'14:52:16',
                 "end_time"=>'14:59:16',
@@ -117,7 +117,7 @@ class espScheduleTableSeeder extends Seeder
                ]);
         DB::table('espschedules')->insert([  
                 "id"=>5,
-                "doctor_id" =>$max_id_doctor-2,
+                "doctor_id" =>$max_id_doctor+2,
                 "date"=>'2019-07-12 ',
                 "start_time"=>'14:52:16',
                 "end_time"=>'14:59:16',
