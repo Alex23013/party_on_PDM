138c7be1ba98cf781ca73dec0a87390fe13f7ba7
Maria <alecornejolupa2013@gmail.com>
2019-07-15 10:37:57 -0500

    fix: add tech form

--- resources/views/techs/new_tuser.blade.php
+++ resources/views/techs/new_tuser.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Registrar un Téncico</div>
+                <div class="panel-heading">Registrar un Técnico</div>
                 <div class="panel-body">
                 @if (count($errors) > 0)
                       <div class="alert alert-danger">
