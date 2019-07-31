c3a99ae2af5266fbb38614f7b867a18da849b402
Maria <alecornejolupa2013@gmail.com>
2019-07-30 20:09:35 -0500

    agregar token y estado de pagado o no => tabla: dservices

---
+++ database/migrations/2019_07_30_200508_add_token_and_pay_status_to_dservices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTokenAndPayStatusToDservicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('dservices', function (Blueprint $table) {
+            $table->boolean('payment_status')->default(0);
+            $table->string('token_pay')->default("undefined");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       Schema::table('dservices', function (Blueprint $table) {
+            $table->dropColumn('payment_status');
+            $table->dropColumn('token_pay');
+        });
+    }
+}
