049de2457d2a185231dbb04b45b3eccab91dcd9b
maria <alecornejolupa2013@gmail.com>
2019-08-13 13:18:03 -0500

    mapa en new dservice module

--- resources/views/docdoor_services/d_services_detail.blade.php
+++ resources/views/docdoor_services/d_services_detail.blade.php
@@ -11,13 +11,21 @@
     padding-left: 5%;
   }
 </style>
+<style type="text/css">
+  .p-left{
+    padding-left: 5%;
+  }
+  #map {
+        height: 400px;
+      }
+</style>
 
 <div class="row">
   <div class="col-xs-12">
     <div class="box">
       <div class="box-body p-left">
       <h3> Información de la Solicitud de Servicio DocDoor<br></h3>
-      <div class="col-md-8">
+      <div class="col-md-6">
         <div class="col-md-12" >
           <span class="col-md-4"> Paciente: </span>
           <label  class="col-md-8">{{$user->name}} </label>
@@ -67,8 +75,10 @@
           @endif
         </div>
        </div>
-      <div class="col-md-4 ">
-            <img src="/images/solicitud.svg" class= "imgAvatar">
+      <div class="col-md-6">
+        <div class="col-md-12 ">  
+          <div id="map" class="m-left"></div>
+        </div>             
       </div>  
       
         <div class="col-md-12 ">
@@ -88,4 +98,24 @@
     </div>
   </div>
 </div>
+@endsection
+
+@section('specific scripts')
+<script>
+     function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: {lat: <?php echo $data->address_to_latitude?>    ,lng:<?php echo $data->address_to_longitude?> }
+        });
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(<?php echo $data->address_to_latitude?>, <?php echo $data->address_to_longitude?>),
+          map: map,
+          title: "Dirección de llegada de la solicitud",          
+        });
+      }
+
+    </script>
+    <script async defer
+    src="https://maps.google.com/maps/api/js?key=AIzaSyCILxmzsVKpgprW3wmiVyBk3-ylNy2g8Vc&callback=initMap">
+    </script>
 @endsection
